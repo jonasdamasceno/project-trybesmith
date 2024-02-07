c61b3d76fce6993c06cf6f64bf6c605d4869f9f0
jdamasceno <jonasmichael2021@outlook.com>
2024-02-07 01:34:05 -0300

    requisito 5

---
+++ src/middlewares/validateProductPrice.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+
+const validateProductPrice: RequestHandler = (req, res, next) => {
+  const { price } = req.body;
+  if (!price) {
+    return res.status(400).json({ message: '"price" is required' });
+  }
+  if (typeof price !== 'string') {
+    return res.status(422).json({ message: '"price" must be a string' });
+  }
+  if (price.length < 3) {
+    return res
+      .status(422)
+      .json({ message: '"price" length must be at least 3 characters long' });
+  }
+  next();
+};
+
+export default validateProductPrice;
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsController, { createProductHandler } from '../controller/products.controller';
+import validadeProductName from '../middlewares/validateProductName';
+import validateProductPrice from '../middlewares/validateProductPrice';
 
 const productsRoute = Router();
 
-productsRoute.post('/', createProductHandler);
+productsRoute.post('/', validadeProductName, validateProductPrice, createProductHandler);
 productsRoute.get('/', productsController.getAllProductsController);
 
 export default productsRoute;
